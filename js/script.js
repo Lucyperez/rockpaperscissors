2af92f07d2b147186178cae29505b6c61dd05eb4
Lucia <s_lucia.perezmendoza@ousd.org>
2018-10-25 01:01:22 +0000

    added nes variables

--- js/script.js
+++ js/script.js
@@ -6,14 +6,46 @@
 /* global $ */
 
 
+// var userChoice ="";
+var userChoice;
+var computerChoice;
+var winner;
+var randomNumber;
+
+userChoice = "";
+computerChoice = "";
+winner = "";
+randomNumber= Math.random();
+
 
 // DOCUMENT READY FUNCTION BELOW
 $("#shoot").click(function() {
     // body...
     $("#userChoice").text($("#input").val());
+    var userChoice = $("").val();  
+    randomNumber = Math.random();
+    
+    
 })
 
-$("#choices").click(function(){
-    $("#computerChoice").text($("#input").val());
-})
+
+
+    // $("#computerChoice").text('test');
+    if ( randomNumber > 0.6)
+    {
+         computerChoice= "rock";
+    }
+    
+    else if (randomNumber > 0.3)
+    {
+        computerChoice = "paper";
+    }
+    
+    else {
+        computerChoice = "scissors";
+    }
+     $("#computerChoice").text(computerChoice);
+    
+
+
 
